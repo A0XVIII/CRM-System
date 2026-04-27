19501de11c51f2688d6d56bf6caa9c6198027822
A0XVIII <166369816+A0XVIII@users.noreply.github.com>
2026-04-27 17:53:28 +0300

    Убрал alert при ошибке, теперь появляется предупреждающий текст

--- src/components/AddTodoForm.jsx
+++ src/components/AddTodoForm.jsx
@@ -3,16 +3,18 @@ import { addTodo } from "../api/taskAPI";
 
 export default function AddTodoForm({ updateTodos }) {
   const [todoTitle, setTodoTitle] = useState("");
+  const [error, setError] = useState ("");
 
   const validateValue = (value) => {
     if (value.trim() === "") {
-      alert("Введите значение");
+      setError("Введите значение");
       return false;
     }
     if (value.trim().length < 2 || value.trim().length > 64) {
-      alert("Введите значение от 2 до 64 символов");
+      setError("Введите значение от 2 до 64 символов");
       return false;
     }
+    setError("");
     return true;
   };
 
@@ -30,12 +32,15 @@ export default function AddTodoForm({ updateTodos }) {
   };
   return (
     <form onSubmit={handleSubmit}>
+      <div>
       <input
         onChange={(e) => setTodoTitle(e.target.value)}
         type="text"
         placeholder="Добавьте задачу"
         value={todoTitle}
       />
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      </div>
       <button type="submit">Добавить задачу</button>
     </form>
   );
